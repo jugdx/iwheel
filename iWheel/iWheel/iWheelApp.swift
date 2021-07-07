6528fe21dfba84321a9f3774ed20511e8f1442e5
Charlotte Martin <charlotte.martin@blablacar.com>
2021-07-07 10:04:55 +0200

    Create project

---
+++ iWheel/iWheel/iWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWheelApp.swift
+//  iWheel
+//
+//  Created by Charlotte Martin on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
