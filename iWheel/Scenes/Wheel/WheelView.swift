1a4d7f4380ca78df661d104258d250cef9776137
Julien Goudeaux <julien.goudeaux@blablacar.com>
2021-07-07 10:44:23 +0200

    Update repository folders

---
+++ iWheel/Scenes/Wheel/WheelView.swift
@@ -0,0 +1,20 @@
+//
+//  WheelView.swift
+//  iWheel
+//
+//  Created by Julien Goudeaux on 07/07/2021.
+//
+
+import SwiftUI
+
+struct WheelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WheelView_Previews: PreviewProvider {
+    static var previews: some View {
+        WheelView()
+    }
+}
